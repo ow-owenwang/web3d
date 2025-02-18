c08aab56044c0147edd9053bc17ef51c9291b7b9
e_wangyy <e_wangyy@enn.cn>
2025-02-18 17:24:31 +0800

    update

--- cesium-code/src/utils/index.ts
+++ cesium-code/src/utils/index.ts
@@ -3,7 +3,10 @@ import { Ion, Viewer } from "cesium";
 const ACCESS_TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiODM2NzczYS04NDhmLTQyNTYtYjkwOS0zODdiODlmOWYzY2QiLCJpZCI6MjYyMzk1LCJpYXQiOjE3MzQxNjE1NzV9.QDNVMAkaZB66ctF29zEO8njoVdxykBprPqVesf5e-aM";
 
-export function createViewer(domEl: string = 'cesiumContainer', options: any = {}) {
+export function createViewer(
+  domEl: string = "cesiumContainer",
+  options?: Viewer.ConstructorOptions
+) {
   Ion.defaultAccessToken = ACCESS_TOKEN;
 
   // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
